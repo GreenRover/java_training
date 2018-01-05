31910b764436be081d8634f21e417c23e5e0cd1f
Heiko Henning <heiko.henning@mtrail.ch>
2018-01-05 15:34:00 +0100

    Add first hibernate model

--- src/main/java/ch/mtrail/demo/App.java
+++ src/main/java/ch/mtrail/demo/App.java
@@ -1,13 +1,33 @@
 package ch.mtrail.demo;
 
+import org.hibernate.Session;
+
+import ch.mtrail.demo.model.Stock;
+import ch.mtrail.demo.persistence.HibernateUtil;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( final String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println("Maven + Hibernate + MySQL");
+    	final Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            final Stock stock = new Stock();
+
+            stock.setStockCode("4715");
+            stock.setStockName("GENM");
+
+            session.save(stock);
+            session.getTransaction().commit();
+        } finally {
+        	session.close();
+        	session.getSessionFactory().close();
+		}
+        
     }
 }
