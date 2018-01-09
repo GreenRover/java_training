eef69c641a0093b032f3f3f6a2cee2f3cfcefe5e
Heiko Henning UE85540 <heiko.henning@sbb.ch>
2018-01-09 10:50:09 +0100

    Add zugfahrt hbm and model

--- src/main/java/ch/mtrail/demo/App.java
+++ src/main/java/ch/mtrail/demo/App.java
@@ -1,33 +1,38 @@
 package ch.mtrail.demo;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
-import ch.mtrail.demo.model.Stock;
+import ch.mtrail.demo.model.Zugfahrt;
+import ch.mtrail.demo.persistence.CloseableSession;
 import ch.mtrail.demo.persistence.HibernateUtil;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( final String[] args )
-    {
-    	System.out.println("Maven + Hibernate + MySQL");
-    	final Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            session.beginTransaction();
-            final Stock stock = new Stock();
+public class App {
+	public static void main(final String[] args) {
+		System.out.println("Maven + Hibernate + MySQL");
+		try (final CloseableSession closeableSession = new CloseableSession(HibernateUtil.getSessionFactory().openSession())) {
+			final Session session = closeableSession.delegate();
+			
+			session.beginTransaction();
 
-            stock.setStockCode("4715");
-            stock.setStockName("GENM");
+			final Query<Zugfahrt> query = session.createQuery("SELECT z FROM Zugfahrt z " + //
+					" LEFT JOIN z.zugfahrtSollpunkte sp " + //
+					" WHERE z.zugNr = :zugNr AND sp.bp = :bp" + //
+					" GROUP BY z.trainId", Zugfahrt.class);
+			query.setParameter("zugNr", 540);
+			query.setParameter("bp", "DT");
 
-            session.save(stock);
-            session.getTransaction().commit();
-        } finally {
-        	session.close();
-        	session.getSessionFactory().close();
+			for (final Zugfahrt zugfahrt : query.getResultList()) {
+				System.out.println(zugfahrt);
+			}
+
+			// Nothing to commit / nothing changed.
+			session.getTransaction().rollback();
 		}
-        
-    }
+
+	}
 }
